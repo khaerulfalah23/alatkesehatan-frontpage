b98b8fd0c849bcdd93a7538e8af536072bb05bf6
khaerulfalah23 <khaerulfalah23@gmail.com>
2026-01-06 12:01:16 +0700

    feat: update axios baseURL to production endpoint

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://alatkesehatan.test',
+  baseURL: 'https://slategray-hawk-192959.hostingersite.com',
 });
