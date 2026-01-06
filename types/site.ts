5e7af7dce352e47d6a40709921f5e80cf38d4b85
khaerulfalah23 <khaerulfalah23@gmail.com>
2026-01-06 16:22:41 +0700

    feat: update site domain to new production URL

--- types/site.ts
+++ types/site.ts
@@ -7,5 +7,6 @@ type SiteConfig = {
 export const siteConfig: SiteConfig = {
   site_name: 'Alat Kesehatan',
   site_description: 'Aplikasi katalog alat kesehatan sederhana.',
-  site_domain: 'https://alatkesehatan-frontpage.vercel.app',
+  // site_domain: 'https://alatkesehatan-frontpage.vercel.app',
+  site_domain: 'https://tubesettlerlamella.com',
 };
